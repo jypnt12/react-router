0f728ffb66758902f6ea09c3d8fb2a446e95819c
jaypante <jpante.pbox@parsu.edu.ph>
2023-02-13 21:10:11 +0800

    6. Adding Nav and styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import AboutUs from "./pages/AboutUs";
 //GlobalStyle
 import GlobalStyle from "./components/GlobalStyle";
 
+import Nav from "./components/Nav";
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
