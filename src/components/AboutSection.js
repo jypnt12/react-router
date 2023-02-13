620cf83eaa1c0be2ff0244c4ede4e6890aa3db73
jaypante <jpante.pbox@parsu.edu.ph>
2023-02-13 20:37:55 +0800

    5. Styling services section and separating style

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import home1 from '../img/home1.png'
 import styled from 'styled-components'
+import {About, Description, Image, Hide} from '../styles'
 
 const AboutSection = () => {
   return (
@@ -30,35 +31,35 @@ const AboutSection = () => {
 //styled component
 //install vscode styled-components extension for styling
 
-const About = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
+// const About = styled.div`
+//     min-height: 90vh;
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-between;
+//     padding: 5rem 10rem;
+//     color: white;
+// `;
 
-const Description = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2{
-        font-weight: lighter;
-    }
-`;
+// const Description = styled.div`
+//     flex: 1;
+//     padding-right: 5rem;
+//     h2{
+//         font-weight: lighter;
+//     }
+// `;
 
-const Image = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
+// const Image = styled.div`
+//     flex: 1;
+//     overflow: hidden;
+//     img{
+//         width: 100%;
+//         height: 80vh;
+//         object-fit: cover;
+//     }
+// `;
 
-const Hide = styled.div`
-    overflow: hidden;
-`
+// const Hide = styled.div`
+//     overflow: hidden;
+// `
 
 export default AboutSection
