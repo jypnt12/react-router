64e736a8959e4622d09fd479a93ac6c2a8fb723e
jaypante <jpante.pbox@parsu.edu.ph>
2023-02-13 20:51:20 +0800

    5.5 styling faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,41 +1,70 @@
 import React from 'react'
+import styled from 'styled-components'
+import { About } from '../styles'
 
 const FaqSection = () => {
   return (
-    <div className=''>
-        <div className="faq">
+    
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How do I start?</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
-
-                </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
+                 </div>
+                 <div className="faq-line"></div>
                 <div className="question">
                 <h4>Daily schedule</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
-                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>       
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different payment methods</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
-                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Whaat product do you offer</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
-                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione commodi cupiditate ex expedita porro ad doloribus eum natus possimus soluta dolore a mollitia sapiente consequatur dolor, dignissimos aliquam eius obcaecati?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             </div>
-        </div>
-    </div>
+        </Faq>
+   
   )
-}
+};
+
+const Faq =styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`
 
 export default FaqSection
