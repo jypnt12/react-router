31e9ae8d45f6b6376c1cafd907a6c57c76efb333
jaypante <jaypante12@gmail.com>
2023-02-15 10:38:09 +0800

    commit testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,13 +7,13 @@ const Nav = () => {
         <h1><a id='logo' href="#">Capture</a></h1>
         <ul>
             <li>
-                <a href="#">1. About us</a>
+                <a href="#">About us</a>
             </li>
             <li>
-                <a href="#">2. Our work</a>
+                <a href="#">Our work</a>
             </li>
             <li>
-                <a href="#">3. Contact us</a>
+                <a href="#">Contact us</a>
             </li>
         </ul>
         
@@ -46,6 +46,7 @@ const StyledNav = styled.nav`
     li{
         padding-left: 10rem;
         position: relative;
+
     }
 `
 export default Nav
